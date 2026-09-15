2ea18e4d717233fb8f2c24fa8ee7c32c284cd58b
Edoardo henrique <2006edoardo@gmail.com>
2026-09-14 22:10:15 -0300

    fix: adiciona rotas do app events no router principal api_v1_0

--- boilerplatejwt/router/api_v1_0.py
+++ boilerplatejwt/router/api_v1_0.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 from apps.commons.api.v1.router import common_router
 from apps.core.api.v1.router import core_router
+from apps.events.api.v1.router import urlpatterns as events_urls  # <--- Importa a lista de URLs
 from apps.users.api.v1.router import auth_urls, user_router
 
 api_v1_0_urls = [
     path("common/", include((common_router.urls, "common"), namespace="common")),
     path("core/", include((core_router.urls, "core"), namespace="core")),
+    path("events/", include((events_urls, "events"), namespace="events")),  # <--- Inclui as rotas aqui
     path("user/", include((user_router.urls, "user"), namespace="user")),
 ] + auth_urls
